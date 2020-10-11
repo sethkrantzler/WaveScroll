ce2104fca65c834266cb42d67de161e5ea7b5890
Seth Krantzler <sethkrantzler@gmail.com>
2020-10-11 15:03:36 -0700

    Updates

---
+++ precache-manifest.007228344acb78c714bce959397b3204.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a3fff6762889b24820f3fba55273cc6",
+    "url": "/WaveScroll/index.html"
+  },
+  {
+    "revision": "1d43c3e786c65fb12528",
+    "url": "/WaveScroll/static/css/main.78407e8c.chunk.css"
+  },
+  {
+    "revision": "5e7d49f7d9a62273e013",
+    "url": "/WaveScroll/static/js/2.4fb9dbba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WaveScroll/static/js/2.4fb9dbba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d43c3e786c65fb12528",
+    "url": "/WaveScroll/static/js/main.0b9d0bfd.chunk.js"
+  },
+  {
+    "revision": "dd5dc1db1cd78bbd8b06",
+    "url": "/WaveScroll/static/js/runtime-main.497f18b8.js"
+  },
+  {
+    "revision": "3bef329f1e60c2e10cad48ce87aecfe5",
+    "url": "/WaveScroll/static/media/Documents - Shortcut.3bef329f.lnk"
+  },
+  {
+    "revision": "e264fd98363d81cc1925fe62001a8cde",
+    "url": "/WaveScroll/static/media/GreatWaveLayer0.e264fd98.png"
+  },
+  {
+    "revision": "4d291da8210f65fb52b1e9b75b9816f0",
+    "url": "/WaveScroll/static/media/GreatWaveLayer1.4d291da8.png"
+  },
+  {
+    "revision": "24fffb8d0be4c2b7d167f3ebd5fa6c17",
+    "url": "/WaveScroll/static/media/GreatWaveLayer2.24fffb8d.png"
+  },
+  {
+    "revision": "c72a5495a5431206155485520b758eea",
+    "url": "/WaveScroll/static/media/GreatWaveLayer3.c72a5495.png"
+  },
+  {
+    "revision": "18e3f7f9825c9e9263c18b31d802fcee",
+    "url": "/WaveScroll/static/media/GreatWaveLayer4.18e3f7f9.png"
+  },
+  {
+    "revision": "1ce5d9d292ea9a3ccd0a9014b125cace",
+    "url": "/WaveScroll/static/media/JapestylePlain.1ce5d9d2.ttf"
+  },
+  {
+    "revision": "2f3a89293f3aac70e6b05e9007d0bda8",
+    "url": "/WaveScroll/static/media/JapestylePlain.2f3a8929.eot"
+  },
+  {
+    "revision": "710891133c162a8f2fd7d583abefa5f6",
+    "url": "/WaveScroll/static/media/JapestylePlain.71089113.woff2"
+  },
+  {
+    "revision": "bff58d493d1534f00b7ef5f9324b8464",
+    "url": "/WaveScroll/static/media/JapestylePlain.bff58d49.svg"
+  },
+  {
+    "revision": "ccb13abd0dc894d22baa1a7359a2013b",
+    "url": "/WaveScroll/static/media/JapestylePlain.ccb13abd.woff"
+  },
+  {
+    "revision": "292ce0e19c14944b73b30333b861e740",
+    "url": "/WaveScroll/static/media/TestGif.292ce0e1.gif"
+  }
+]);
